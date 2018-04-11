6cd6cd716ad9f06ebf0b721cc882b71b26e29be9
piotrbocian <piotrbocian87@gmail.com>
2018-04-11 06:53:37 +0200

    [cpp] Nawiasy ({[]})

---
+++ cpp/Stos/Nawiasy.cpp
@@ -0,0 +1,52 @@
+#include <iostream> 
+#include <stack>
+using namespace std;
+
+bool isLeft(char c)
+{
+    return (c == '(' || c == '{' || c == '[');
+}
+
+bool isMatching(char left, char right)
+{
+    return (
+        (left == '(' && right == ')') ||
+        (left == '{' && right == '}') ||
+        (left == '[' && right == ']')
+        );
+}
+
+bool isBalanced(string line)
+{
+    std::stack<char> st;
+
+    for (auto c : line)
+    {
+        if (isLeft(c))
+        {
+            st.push(c);
+        }
+        else
+        {
+            if (!st.empty() && isMatching(st.top(), c))
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    return st.empty();
+}
+
+int main()
+{
+    string line = "()(){[]}";
+
+    cout << ((isBalanced(line)) ? "YES\n" : "NO\n");
+
+    return 0;
+}
