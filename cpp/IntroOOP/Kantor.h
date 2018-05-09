eb2caef8ca140ea3d8eebf8eae76d6285c64beff
piotrbocian <piotrbocian87@gmail.com>
2018-05-09 21:41:33 +0200

    kantor - obliczenia

--- cpp/IntroOOP/Kantor.h
+++ cpp/IntroOOP/Kantor.h
@@ -4,6 +4,9 @@
 #pragma once
 
 
+#include <iostream>
+using namespace std;
+
 class Kantor
 {
 public:
@@ -15,35 +18,52 @@ public:
     }
     float kupUsd(float pln)
     {
+        // pierwsza forma obliczen
+        float kursWymiany = USD_PLN * (1 + SPREAD / 2);
+        zysk += pln - pln / (1 + SPREAD / 2);
+        return pln / kursWymiany;
     }
 
     float kupEur(float pln)
     {
+        // druga forma obliczen
+        float kursWymiany = 1 / (EUR_PLN * (1 + SPREAD / 2));
+        zysk += pln * (1 / EUR_PLN - kursWymiany) * EUR_PLN;    // zysk w EUR * kurs EUR
+        return pln * kursWymiany;
     }
 
-    float sprzedajUsd(float pln)
+    float sprzedajUsd(float usd)
     {
+        float kursWymiany = (USD_PLN * (1 - SPREAD / 2));
+        zysk += usd * (USD_PLN - kursWymiany);
+        return usd * kursWymiany;
     }
 
-    float sprzedajEur(float pln)
+    float sprzedajEur(float eur)
     {
+        float kursWymiany = (EUR_PLN * (1 - SPREAD / 2));
+        zysk += eur * (EUR_PLN - kursWymiany);
+        return eur * kursWymiany;
     }
 
 private:
     float USD_PLN;
     float EUR_PLN;
     float SPREAD;
+    float zysk = 0;
 };
 
-void main()
+void kantor_test()
 {
     Kantor k(3.40, 4.30);
     float pln = 100;
-    float usd = k.kupUsd(usd);
+    float usd = k.kupUsd(pln);
     pln = k.sprzedajUsd(usd);
 
     float eur = k.kupEur(pln);
     pln = k.sprzedajEur(eur);
 
     float zarobek = 100 - pln;
+
+    cout << "Zarobek " << zarobek << endl;
 }
