f26611870f21010df848b07396ea602c540fe1ef
piotrbocian <piotr.bocian@intel.com>
2018-06-02 15:54:48 +0200

    [cpp][cs] Exceptions - more comments

--- cs/Exceptions/ProgramExceptions.cs
+++ cs/Exceptions/ProgramExceptions.cs
@@ -1,34 +1,59 @@
-﻿using System;
+﻿// Copyright 2018 Piotr Bocian
+// Opracowanie Piotr Bocian
 
+using System;
+
+// dodatkowe materiały:
+// https://www.tutorialspoint.com/csharp/csharp_exception_handling.htm
 namespace Exceptions
 {
+    // deklaracja wyjątku przez dziedziczenie z wyjątku wbudowanego
+    // spójrz na funkcję niżej (int podziel(a, b)) po przykład użycia
     class DivideByZeroException : Exception
     {
+        // wywołujemy konstruktor bazowy gdzie nadpisujemy parametr 'message'
         public DivideByZeroException() : base("Nie wolno dzielic przez 0!!!") { }
     }
 
-
-    class Program
+    class ProgramExceptions
     {
-
+        // Funkcja do dzielenia odporna na dzielenie przez zero
         static int podziel(int a, int b)
         {
+            // gdy pojawi się sytuacja błędna, dobrym sposobem zasygnalizowania tego użytkownikowi jest
+            // "rzucenie wyjątku" (ang. 'throw exception')
             if (b == 0)
             {
+                // możemy tutaj użyć wbudowanego typu std::exception albo rzucić własnym wyjątkiem:
+
+                // wyjątek wbudowany:
                 //throw new Exception("Nie wolno dzielic przez 0!!!");
+
+                // wyjątek zdefiniowany przez nas:
                 throw new DivideByZeroException();
             }
 
             return a / b;
         }
 
+        // Przykłady użycia powyższych funkcji
         static void Main(string[] args)
         {
+            // wyjątek nieobsłużony:
+            //podziel(5, 0);
+
+            // obsługa wyjątków w sekcji try{} catch{}
+            // try{} - umieszczamy tutaj fragment kodu gdzie możemy otrzymać wyjątek
             try
             {
                 podziel(5, 0);
+
+                // Przykład - propagacja wyjątków
+                // wyjątek zostanie spropagowany z funkcji podziel() do fragmentWiekszegoProgramu() i dopiero w main() go złapiemy
                 fragmentWiekszegoProgramu();
             }
+            // catch(){} - wyłapujemy określone wyjątki
+            // wyjątki łapiemy w kolejności od szczegółowych do ogólnych
             catch (DivideByZeroException e)
             {
                 Console.WriteLine(e.Message);
@@ -38,19 +63,24 @@ namespace Exceptions
                 Console.WriteLine(e.Message);
             }
 
-            Kalkulator k = new Kalkulator();
-            k.dodaj();
-            k.Dispose();
-
-            using (Kalkulator k2 = new Kalkulator())
+            // Przykład - zastosowanie wyjątków w konstuktorze
+            try
             {
-                k2.dodaj();
-                k2.dodaj();
+                Kalk k = new Kalk();
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
 
+        // Funkcja służąca do ilustracji 'propagacji wyjątków'.
+        // Funkcja ta woła funkcję podziel(a, b) gdzie powstanie wyjątek z powodu dzielenia przez 0
+        // ale ten wyjątek nie zostanie tutaj obsłużony tylko zostanie przesłany wyżej
         static void fragmentWiekszegoProgramu()
         {
+            // ...
             int a = 7;
             int b = 0;
             double wynik = podziel(a, b);
@@ -97,5 +127,17 @@ namespace Exceptions
         //   {
         //     ...
         //   }
+    }
+
+    // Zastosowanie wyjątków w konstuktorach
+    // Jak wiemy, funkcja konstruktora nie zwraca żadnego typu,
+    // więc w jaki sposób powiedzieć, że coś poszło nie tak? Rzucić wyjątkiem.
+    class Kalk
+    {
+        public Kalk()
+        {
+            // coś w konstuktorze poszło nie tak...
+            throw new Exception("wyjątek w konstuktorze");
+        }
     }
 }
